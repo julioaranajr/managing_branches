fcb920ea7529db0695141b66f643ece584a0c46f
julioaranajr <julioarana.de@gmail.com>
2024-05-16 11:29:47 +0200

    conflicts will happen

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 # For a straightforward and quintessentially Pythonic way to showcase the "Hello, World!" program in Python, 
 # we turn to the trusty print() function to print(hello world in Python using a function). 
 # Observe this code snippet for a glimpse into its simplicity: print("Hello, World!")
-print("Hello, World!")
+
+print("Hello!")
+print ("Hello, Amazing People!")
