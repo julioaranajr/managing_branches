4e384fd672520767754e233d6af4f25a1f63d476
julioaranajr <julioarana.de@gmail.com>
2024-05-16 11:13:42 +0200

    adding python file

---
+++ hello.py
@@ -0,0 +1,4 @@
+# For a straightforward and quintessentially Pythonic way to showcase the "Hello, World!" program in Python, 
+# we turn to the trusty print() function to print(hello world in Python using a function). 
+# Observe this code snippet for a glimpse into its simplicity: print("Hello, World!")
+print("Hello, World!")
